1b5e6aa3855d8a9cbfec158b7b8fe12740270702
Hintertuxer <jrinman@calpoly.edu>
2024-10-25 09:24:17 -0700

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -3,7 +3,19 @@ import data
 # Write your functions for each part in the space below.
 
 # Part 1
-
+def vowel_count(input_string: str) -> int:
+    """
+    Counts the number of vowels (a, e, i, o, u) in the input string.
+    
+    Inputs:
+        - input_string (str): The string to check for vowels.
+    
+    Output:
+        - int: The count of vowels in the input string.
+    """
+    vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+    count = sum(1 for char in input_string if char in vowels)
+    return count
 
 # Part 2
 
